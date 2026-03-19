1d15d8b2ad0c96f9107629ab28538f66a39c9525
joaoleonardome-dotcom <joaoleonardo.me@gmail.com>
2026-03-19 20:53:04 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'arena-olimpica-v2';
+const CACHE = 'arena-olimpica-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
